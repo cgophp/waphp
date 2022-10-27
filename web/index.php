d40d966647fbdf5f2c13a1df133775bacc9b0654
cgophp <549777430@qq.com>
2022-10-28 03:14:47 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,11 +3,8 @@
 define('WA_START_TIME', microtime(true));
 
 ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
 error_reporting(E_ALL);
 
-set_time_limit(0);
-ini_set('memory_limit', '-1');
 date_default_timezone_set('PRC');
 
 const WA_WEB_PATH = __DIR__;
